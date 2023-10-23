109dcd6828fc5aebae5967cd377f47ca9e878d92
pthanh07 <bimbimdethuong167@gmail.com>
2023-10-23 16:29:37 +0700

    Ham tong

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	return a + b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout << tong(3, 5);
 	system("pause");
 	return 0;
 }
